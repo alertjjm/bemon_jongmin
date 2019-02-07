c91b2631f68350f8434da95822361eedb7f95284
alertjjm <sdr2936@gmail.com>
2019-02-07 17:28:59 +0900

    190207 socket commit

---
+++ signal.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<signal.h>
+
+void timeout(int sig){
+	if(sig==SIGALRM)
+		puts("TIME OUT!");
+	alarm(2);
+}
+void keycontrol(int sig){
+	if(sig==SIGINT)
+		puts("CTRL+C pressed");
+}
+int main(int argc, char* argv[]){
+	int i;
+	signal(SIGALRM, timeout);
+	signal(SIGINT, keycontrol);
+	alarm(2);
+	for(i=0; i<3; i++){
+		puts("wait...");
+		sleep(100);
+	}
+	return 0;
+}
