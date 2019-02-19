555176bd178144f8fb5c914ef0aed29bee0cde37
alertjjm <sdr2936@gmail.com>
2019-02-19 23:20:32 +0900

    190219 assign[14]

--- Bemon_C++과제/14/14.cpp
+++ Bemon_C++과제/14/14.cpp
@@ -4,82 +4,57 @@
 #include "stdafx.h"
 #include <iostream>
 #include <string>
+#include <iostream>
 using namespace std;
 
 class ArrayUtility2 {
-
 public:
-	ArrayUtility2(int s){
-		x = new int[s];
-		y = new int[s];
-	}
 	static int* concat(int s1[], int s2[], int size) {
-		int* result = new int[size * 2];
-		for (int i = 0; i < size; i++) {
-			result[i] = s1[i];
+		int* p = new int[size];
+		int n = size / 2;
+		for (int i = 0; i< n; i++) {
+			p[i] = s1[i];
+			p[i + n] = s2[i];
 		}
-		for (int i = 0; i < size; i++) {
-			result[i + 5] = s2[i];
-		}
-		return result;
+		return p;
 	}
 	static int* remove(int s1[], int s2[], int size, int& retSize) {
-		int i,j;
-		int index;
-		int num = 0;
-		retSize = size;
-		for (i = 0; i < size; i++) {
-			for (j = 0; j < size; j++) {
-				if (s1[i] == s2[j]) {
-					retSize--;
-				}
+
+		for (int i = 0; i< size; i++) {
+			for (int j = 0; j< size; j++) {
+				if (s1[i] == s2[j]) s1[i] = NULL;
 			}
 		}
-		int* result = new int[retSize];
-		for (i = 0; i < size; i++) {
-			index = 0;
-			for (j = 0; j < size; j++) {
-				if (s1[i] == s2[j]) {
-					index++;
-				}
-			}
-			if (index == 0) {
-				result[num] = s1[i];
-				num++;
-			}
-		}
-		if (retSize == 0)
-			return NULL;
-		return result;
+		for (int i = 0; i< size; i++)
+			if (s1[i] != NULL) retSize++;
+
+		int *p = new int[retSize];
+		int n = 0;
+		for (int i = 0; i< size; i++)
+			if (s1[i] != NULL) { p[n] = s1[i]; n++; }
+
+		if (retSize == 0) return NULL;
+		else return p;
 	}
-	int* x = nullptr;
-	int* y = nullptr;
 };
 
+int main() {
+	int x[5], y[5], retsize = 0, size;
 
-int main()
-{
-	int size_m = 5;
-	int ret = 0;
-	int i;
-	string strx;
-	string stry;
-	ArrayUtility2 a(size_m);
-	int* result1;
-	int* result2;
-	cout << "정수를 " << size_m << "개 입력하라. 배열 x에 삽입한다. >> " << endl;
-	getline(cin, strx);
-	for (i = 0; i < size_m; i++) {
-		a.x[i] = strx[i * 2];
-	}
-	getline(cin, stry);
-	for (i = 0; i < size_m; i++) {
-		a.y[i] = stry[i * 2];
-	}
-	result1 = a.concat(a.x, a.y, size_m);
-	result2 = a.remove(a.x, a.y, size_m, ret);
-	cout << "합친 정수 배열을 출력한다.\n" << result1 << endl;
-	cout << "배열 x[]에서 y[]를 뺀 결과를 출력한다. 개수는 " << ret << "\n" << result2 << endl;
-    return 0;
+	cout << "정수를 5 개 입력하라. 배열 x에 삽입한다>>";
+	cin >> x[0] >> x[1] >> x[2] >> x[3] >> x[4];
+	cout << "정수를 5 개 입력하라. 배열 y에 삽입한다>>";
+	cin >> y[0] >> y[1] >> y[2] >> y[3] >> y[4];
+	cout << "합친 정수 배열을 출력한다." << endl;
+
+	size = sizeof(x) / 4 + sizeof(y) / 4;
+	int *temp = ArrayUtility2::concat(x, y, size);
+	for (int i = 0; i< size; i++) cout << temp[i] << ' ';
+	cout << endl;
+
+	size = sizeof(x) / 4;
+	temp = ArrayUtility2::remove(x, y, size, retsize);
+	cout << "배열 x[]에서 y[]를 뺀 결과를 출력한다. 개수는 " << retsize << endl;
+	for (int i = 0; i< retsize; i++) cout << temp[i] << ' ';
+	cout << endl;
 }
-
