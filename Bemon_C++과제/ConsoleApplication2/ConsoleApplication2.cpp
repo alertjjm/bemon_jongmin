0221b0afd1f77eea32ea7179c45c35ac32c4e32a
alertjjm <sdr2936@gmail.com>
2019-08-13 15:32:23 +0900

    190813

--- Bemon_C++과제/ConsoleApplication2/ConsoleApplication2.cpp
+++ Bemon_C++과제/ConsoleApplication2/ConsoleApplication2.cpp
@@ -2,10 +2,30 @@
 //
 
 #include "stdafx.h"
-
-
-int main()
+#include <iostream>
+using namespace std;
+class CTest {
+	int m_nData;
+public:
+	CTest(int nParam) :m_nData(nParam) {
+		cout << "CTest::CTest()" << endl;
+	}
+	~CTest() {
+		if (m_nData == 1) {
+			cout << "~CTest::CTest() A" << endl;
+		}
+		else {
+			cout << "~CTest::CTest() B" << endl;
+		}
+	}
+};
+int _tmain(int argc, _TCHAR* argv[])
 {
+	cout << "Begin" << endl;
+	CTest a(1);
+	cout << "Before b" << endl;
+	CTest b(2);
+	cout << "End" << endl;
     return 0;
 }
 
