13109fdcd3206c8e088c79fde4f2af6b22c30042
alertjjm <sdr2936@gmail.com>
2019-08-13 15:12:16 +0900

    190813

---
+++ Bemon_C++과제/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication1.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int aList[5] = { 10,20,30,40,50 };
+	for (auto &n : aList)
+		cout << n << ' ';
+	cout << endl;
+    return 0;
+}
+
